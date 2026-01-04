7b11fbfd6583b8d1cab654c4d33bfd708f738da3
thyme <unknown>
2026-01-04 09:11:55 -0500

    Oops! Forgot to add this file! Also, forgot to note the FPS. FPS: 235

---
+++ inc/user.h
@@ -0,0 +1,18 @@
+#ifndef USER_H_
+#define USER_H_
+
+#include "raylib.h"
+#include "grid.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+extern struct Brush
+{
+    uint8_t size;
+    uint8_t value;
+
+} brush;
+
+extern void user_ctrl();
+
+#endif // USER_H_
